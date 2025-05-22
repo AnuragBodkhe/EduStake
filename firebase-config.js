ec150a66b4df15b5ebf1eb51fc889ebfd6d0ddc3
Anurag <bodkheanurag235@gmail.com>
2025-05-23 00:38:11 +0530

    removing api keys from the codes

--- firebase-config.js
+++ firebase-config.js
@@ -9,14 +9,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-hLnUmU12N9ivvIq7efYLe16O4DSLFx4",
-  authDomain: "edustake-ca45d.firebaseapp.com",
-  databaseURL: "https://edustake-ca45d-default-rtdb.firebaseio.com",
-  projectId: "edustake-ca45d",
-  storageBucket: "edustake-ca45d.appspot.com",
-  messagingSenderId: "885060407436",
-  appId: "1:885060407436:web:90227dc8a8f582a8726ba9",
-  measurementId: "G-R7M8MT3BN6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
